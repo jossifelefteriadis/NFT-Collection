23b269f803c50352aa0db68bffcecac4bfd78768
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2022-07-08 13:23:35 +0300

    Fix comments

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
-// Address of the Whitelist Contract that you deployed
+// Address of the Whitelist Contract that's deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x900c08Fd7Ac99Dd530558386886a67C756322c0B";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
